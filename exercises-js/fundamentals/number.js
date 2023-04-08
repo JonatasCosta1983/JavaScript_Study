737f6fa243d78ef8f033b49bc9979192529451c7
JonatasCosta1983 <jonathanevescosta@gmail.com>
2023-04-08 14:18:45 -0300

    module number completed

---
+++ exercises-js/fundamentals/number.js
@@ -0,0 +1,13 @@
+const number1 = 1.0
+const number2 = Number("2.0")
+
+console.log(number1)
+console.log(typeof number1)
+
+console.log(number2)
+console.log(typeof number2)
+
+console.log(Number.isInteger(number1))
+
+console.log(number2.toString())
+console.log(typeof number2)
