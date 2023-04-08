c08d70ddaa410ea9c4b16882dd60828a1313867e
JonatasCosta1983 <jonathanevescosta@gmail.com>
2023-04-08 14:09:43 -0300

    completed comment module

---
+++ exercises-js/fundamentals/comments.js
@@ -0,0 +1,18 @@
+// Comentário de uma única lina
+console.log("Comentário de uma única linha")
+
+
+/*
+Comentários 
+de multiplas 
+linhas
+*/
+console.log("Comentário de multiplas linhas sem o asteristico")
+
+/*
+*Comentário 
+*com 
+*o 
+*asterisco
+*/
+console.log("Comentário com o asterisco")
