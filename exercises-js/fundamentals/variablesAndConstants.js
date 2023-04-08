628832a7ec1fd495f1db5e82c0b1fcb62249e31b
JonatasCosta1983 <jonathanevescosta@gmail.com>
2023-04-08 14:13:14 -0300

    completed variables and constants module

---
+++ exercises-js/fundamentals/variablesAndConstants.js
@@ -0,0 +1,26 @@
+// var palavra reservada para declarar variáves 
+var a = 3
+
+// let palavra reservada para declarar variaves só que a palavra let não pode ser declarada novamente
+
+let b = 4
+
+console.log(a)
+console.log(b)
+
+var a = 30
+ b = 40
+
+ console.log(a)
+ console.log(b)
+
+ a = 300
+ b = 400
+
+ console.log(a)
+ console.log(b)
+
+ const c = 5
+
+ console.log(c)
+
