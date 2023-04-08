4ac298441a77f712a0085f07201abebd99b07a9d
JonatasCosta1983 <jonathanevescosta@gmail.com>
2023-04-08 14:29:39 -0300

    completed object module

---
+++ exercises-js/fundamentals/object.js
@@ -0,0 +1,19 @@
+/*Objeto é um conjunto de chave e valor
+Objeto é diferente de Json
+*/
+const dadoPessoal = {}
+console.log(typeof dadoPessoal)  
+
+dadoPessoal.nome = "Jonatas"
+dadoPessoal.idade = 40
+dadoPessoal.profissao = "Programador"
+
+console.log(dadoPessoal)
+
+const dadoPessoal1 = {
+    nome : "Magda",
+    idade: 41,
+    profissao: "Optometrista"
+}
+
+console.log(typeof dadoPessoal1);
