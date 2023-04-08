c0217374b9fed3610bf9a46b120d66fd9e566fee
JonatasCosta1983 <jonathanevescosta@gmail.com>
2023-04-08 14:22:13 -0300

    completed string module

---
+++ exercises-js/fundamentals/string.js
@@ -0,0 +1,12 @@
+const familia = "Jonatas, Magda, Tiffany, Debora"
+const escola = "Cod3"
+const espaco = "------------"
+console.log(familia.split(","))
+console.log(typeof familia.split(","))
+console.log(espaco)
+
+console.log(escola.charAt(0))
+console.log(typeof escola[0])
+console.log(espaco)
+
+console.log(typeof escola.charAt(1))
