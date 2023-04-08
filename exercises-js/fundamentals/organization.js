d3f12989068c6ef67c1a0def145047c980fbfe9a
JonatasCosta1983 <jonathanevescosta@gmail.com>
2023-04-08 14:05:10 -0300

    completed organization module

---
+++ exercises-js/fundamentals/organization.js
@@ -0,0 +1,9 @@
+//Organização do JavaScript é em sentença e em blocos {}
+
+console.log("Sentença fora do bloco")
+console.log("Sentença com ponto e virgula");
+
+{
+    console.log("Sentença dentro do bloco")
+    console.log("Sentença dentro do bloco com ponto e virgula");
+}
