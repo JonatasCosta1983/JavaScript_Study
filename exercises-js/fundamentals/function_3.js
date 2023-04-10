501d0c06cdb7e0c75d30a3ca8d3fb1728da725fd
JonatasCosta1983 <jonathanevescosta@gmail.com>
2023-04-10 14:13:44 -0300

    module function tres completed

---
+++ exercises-js/fundamentals/function_3.js
@@ -0,0 +1,5 @@
+const diamentroLente = (aro, ponte, menorDnp, maiorDiagonal) =>{
+    return  diametro = aro + ponte - menorDnp * 2 + maiorDiagonal + 2  
+}
+
+console.log(diamentroLente(54, 17, 30, 55));
