1c2cca8d536f6b0ff8f58cf66bdc5849e3ec3626
JonatasCosta1983 <jonathanevescosta@gmail.com>
2023-04-10 14:58:25 -0300

    Global Scope 1 ok

---
+++ exercises-js/fundamentals/varGlobalScope_1.js
@@ -0,0 +1,19 @@
+var numero = 1
+
+{
+    var numero = 2
+    console.log(numero)
+}
+
+console.log(numero)
+
+
+
+let numero2 = 2
+
+{
+    let numero2 = 3
+    console.log(numero2);
+}
+
+console.log(numero2);
